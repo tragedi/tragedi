bdedc54bdcd24a092796be4ba54fc04b69ebb7fd
eaz2006 <72036595+eaz2006@users.noreply.github.com>
2021-04-16 18:43:13 +0500

    start 1

--- bot.py
+++ bot.py
@@ -1,9 +1,8 @@
 import vk_api
 import random
 from vk_api.longpoll import VkLongPoll, VkEventType
-import секрет
 
-vk_session = vk_api.VkApi(token=секрет.token)
+vk_session = vk_api.VkApi(token="f17d2f2e41b3f67ad489d85ce6966c0a42f391f0a30d0ecf730bc46fe019fb8750e9755f09889d08a7849")
 
 vk = vk_session.get_api()
 longpoll = VkLongPoll(vk_session)
